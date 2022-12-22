19244f8d92bcc4b3366178d74bb46ed96db836f9
Alvin Muthui <alvinmuthui@gmail.com>
2022-12-22 19:57:11 +0300

    minor corrections

--- acf/filters-and-actions/actions/acf-save_post.php
+++ acf/filters-and-actions/actions/acf-save_post.php
@@ -3,7 +3,7 @@
 //https://www.advancedcustomfields.com/resources/acf-save_post/#applied-before-save
 add_action('acf/save_post', 'tttc_acf_applied_before_save_post', 5);
 function tttc_acf_applied_before_save_post( $post_id ) {
-    tttc_log('tttc_acf_applied_before_save_post Fired');
+    //tttc_log('tttc_acf_applied_before_save_post Fired');
     // Get previous values.
     $prev_values = get_fields( $post_id );
 
@@ -20,7 +20,7 @@ function tttc_acf_applied_before_save_post( $post_id ) {
 //https://www.advancedcustomfields.com/resources/acf-save_post/#applied-after-save
 add_action('acf/save_post', 'tttc_acf_applied_after_save_post');
 function tttc_acf_applied_after_save_post( $post_id ) {
-    tttc_log('tttc_acf_applied_after_save_post Fired');
+    //tttc_log('tttc_acf_applied_after_save_post Fired');
     // Get newly saved values.
     $values = get_fields( $post_id );
 
--- templates/get-all-fields.php
+++ templates/get-all-fields.php
@@ -10,7 +10,7 @@
 // https://www.advancedcustomfields.com/resources/get_field_objects/
 //a different setup https://drive.google.com/file/d/190Wpa8b7ZlrZgtg3UxswZw5ZY21Kaqhe/view?usp=sharing
 //https://drive.google.com/file/d/1SR6YncUVDC6dz64yR6Zswztah5gB1V9t/view?usp=sharing
-function get_all_fields( $post_id ) {
+function tttc_get_all_fields( $post_id ) {
 	$groups = acf_get_field_groups(  );// retrieve all groups
 
 	foreach ( $groups as &$group ) {
@@ -18,7 +18,7 @@ function get_all_fields( $post_id ) {
 		foreach ( $fields as &$field ) {
 			$field['value'] = get_field( $field['key'], $post_id );// retrieve field value.
 		}
-		 $group['fields'] = $fields;
+		$group['fields'] = $fields;
 	}
 	return $groups;
 
@@ -27,5 +27,5 @@ function get_all_fields( $post_id ) {
 ?>
 
 Custom soln<pre>
-<?php var_export( get_all_fields( false) ); ?>
+<?php var_export( tttc_get_all_fields( false) ); ?>
 </pre>
