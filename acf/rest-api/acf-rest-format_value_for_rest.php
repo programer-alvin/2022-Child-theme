53e2d5149cadecafdef6064926656847c1b87950
Alvin Muthui <alvinmuthui@gmail.com>
2022-12-29 18:06:37 +0300

    Retrieve acf data for posts in post object- REST

--- acf/rest-api/acf-rest-format_value_for_rest.php
+++ acf/rest-api/acf-rest-format_value_for_rest.php
@@ -35,13 +35,24 @@ add_filter(
 add_filter(
 	'acf/rest/format_value_for_rest',
 	function ( $value_formatted, $post_id, $field, $value, $format ) {
-		if ( $field['type'] == 'relationship' ) {
+		if ( 'relationship' === $field['type'] ) {
 			if ( is_array( $value_formatted ) ) {
 				foreach ( $value_formatted as &$post ) {
 					$post->acf = tttc_get_fields_by_post_id( $post->ID );
 				}
 			}
 			return $value_formatted;
+		} elseif ( 'post_object' === $field['type'] ) {
+			if ( is_array( $value_formatted ) ) {// multiple selection is enabled.
+				foreach ( $value_formatted as &$post ) {
+					if ( ! is_int( $post ) ) {// Ensure the only post objects are processed.
+						$post->acf = tttc_get_fields_by_post_id( $post->ID );
+					}
+				}
+			}
+			if ( ! is_int( $post ) ) { // Ensure the only post objects are processed.
+				$value_formatted->acf = tttc_get_fields_by_post_id( $value_formatted->ID );
+			}
 		}
 		return $value_formatted;
 	},
