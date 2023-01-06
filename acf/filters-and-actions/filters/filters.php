38b976ceb8b938aa0e25ec7be5da6d995fa3bd3b
Alvin Muthui <alvinmuthui@gmail.com>
2023-01-06 17:09:42 +0300

    unique field

--- acf/filters-and-actions/filters/filters.php
+++ acf/filters-and-actions/filters/filters.php
@@ -1,4 +1,5 @@
 <?php
-$tttc_acf_filters_dir=trailingslashit( get_stylesheet_directory() ) . 'acf/filters-and-actions/filters';
-require_once $tttc_acf_filters_dir.'/acf-field_group-disable_field_settings_tabs.php';
-require_once $tttc_acf_filters_dir.'/wp_kses_allowed_html.php';
+$tttc_acf_filters_dir = trailingslashit( get_stylesheet_directory() ) . 'acf/filters-and-actions/filters';
+require_once $tttc_acf_filters_dir . '/acf-field_group-disable_field_settings_tabs.php';
+require_once $tttc_acf_filters_dir . '/wp_kses_allowed_html.php';
+require_once $tttc_acf_filters_dir . '/acf-validate_value.php';
