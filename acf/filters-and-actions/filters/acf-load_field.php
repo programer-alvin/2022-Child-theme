253b4f05327e437a5e337c7e4e4a7334981a0cad
Alvin Muthui <alvinmuthui@gmail.com>
2023-02-09 17:36:01 +0300

    Update accordion label using acf/load_field

---
+++ acf/filters-and-actions/filters/acf-load_field.php
@@ -0,0 +1,12 @@
+<?php
+// https://www.advancedcustomfields.com/resources/acf-load_field/
+function tttc_acf_load_field( $field ) {
+	tttc_log( $field );
+    if($field['label']=='accordion'){// label of your accordion field.
+        $field['label'] = get_field('text');// update the label
+    }
+	return $field;
+}
+
+// Apply to accordion fields.
+ add_filter( 'acf/load_field/type=accordion', 'tttc_acf_load_field' );
--- acf/filters-and-actions/filters/filters.php
+++ acf/filters-and-actions/filters/filters.php
@@ -5,3 +5,4 @@ require_once $tttc_acf_filters_dir . '/wp_kses_allowed_html.php';
 require_once $tttc_acf_filters_dir . '/acf-validate_value.php';
 require_once $tttc_acf_filters_dir . '/acf-ajax-shortcode_capability.php';
 require_once $tttc_acf_filters_dir . '/acf-shortcode-allow_in_block_themes_outside_content.php';
+require_once $tttc_acf_filters_dir . '/acf-load_field.php';
