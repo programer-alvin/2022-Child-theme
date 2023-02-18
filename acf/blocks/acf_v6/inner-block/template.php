81be2217951ccc32a9796950004fbb3fbb5d3ee3
Alvin Muthui <alvinmuthui@gmail.com>
2023-02-18 11:14:10 +0300

    added some assets to inner blocks

--- acf/blocks/acf_v6/inner-block/template.php
+++ acf/blocks/acf_v6/inner-block/template.php
@@ -10,42 +10,21 @@
 
 // Create class attribute allowing for custom "className" and "align" values.
 $classes = '';
-if( !empty($block['className']) ) {
-    $classes .= sprintf( ' %s', $block['className'] );
+if ( ! empty( $block['className'] ) ) {
+	$classes .= sprintf( ' %s', $block['className'] );
 }
-if( !empty($block['align']) ) {
-    $classes .= sprintf( ' align%s', $block['align'] );
-}
-
-// Load custom field values.
-$start_date = get_field('start_date');
-$end_date = get_field('end_date');
-
-// Restrict block output (front-end only).
-if( !$is_preview ) {
-    $now = time();
-    if( $start_date && strtotime($start_date) > $now ) {
-        echo sprintf( '<p>Content restricted until %s. Please check back later.</p>', $start_date );
-        return;
-    }
-    if( $end_date && strtotime($end_date) < $now ) {
-        echo sprintf( '<p>Content expired on %s.</p>', $end_date );
-        return;
-    }
+if ( ! empty( $block['align'] ) ) {
+	$classes .= sprintf( ' align%s', $block['align'] );
 }
 
-// Define notification message shown when editing.
-if( $start_date && $end_date ) {
-    $notification = sprintf( 'Content visible from %s until %s.', $start_date, $end_date );
-} elseif( $start_date ) {
-    $notification = sprintf( 'Content visible from %s.', $start_date );
-} elseif( $end_date ) {
-    $notification = sprintf( 'Content visible until %s.', $end_date );
-} else {
-    $notification = 'Content unrestricted.';
-}
+
 ?>
-<div class="restricted-block <?php echo esc_attr($classes); ?>">
-    <span class="restricted-block-notification"><?php echo esc_html( $notification ); ?></span>
-    <InnerBlocks  />
+<div>
+	This block can hold another block.
+</div>
+<div class="inner-block <?php echo esc_attr( $classes ); ?>">
+	<span class="inner-block-notification"><?php echo esc_html( $notification ); ?></span>
+	<div class="inner-block-container">
+		<InnerBlocks  />
+	</div>
 </div>
