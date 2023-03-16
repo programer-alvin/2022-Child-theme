ff9c27dd3e596dbd2a93119504ccfd3bb36ea9b3
Alvin Muthui <alvinmuthui@gmail.com>
2023-03-16 11:19:03 +0300

    register_block_type() using $args

--- acf/blocks/acf_v6/test-block/test-block.php
+++ acf/blocks/acf_v6/test-block/test-block.php
@@ -3,10 +3,22 @@ function tttc_test_block_render_callback() {
 	echo 'tttc_test_block_render_callback Test';
 }
 function tttc_register_test_block() {
-    //https://developer.wordpress.org/reference/classes/wp_block_type/__construct/
+	// https://developer.wordpress.org/reference/classes/wp_block_type/__construct/
 	$args = array(
+		'title'           => 'Test Block Title From Args', // overrides title in block.json
+		'category'        => 'tttc-acf-category',
 		'render_callback' => 'tttc_test_block_render_callback',
+		'description'     => 'Test block description from Args',
+		'keywords'        => array( 'test', 'block', 'args' ),
+		// 'icon'         => 'admin-comments',
+		'supports'        => array(
+			'align'     => false,
+			'alignWide' => false,
+			'html'      => false,
+			//'multiple'  => false,
+		),
+
 	);
-	register_block_type( __DIR__ . '/block.json', $args );//https://developer.wordpress.org/reference/functions/register_block_type/
+	register_block_type( __DIR__ . '/block.json', $args );// https://developer.wordpress.org/reference/functions/register_block_type/
 }
 add_action( 'init', 'tttc_register_test_block' );
