3405b0015e694957fc2a9c13eab02443daeecc94
Alvin Muthui <alvinmuthui@gmail.com>
2023-06-06 11:42:13 +0300

    added shortcode for current post

--- acf/custom-shortcodes/custom-shortcodes.php
+++ acf/custom-shortcodes/custom-shortcodes.php
@@ -5,3 +5,4 @@ $tttc_acf_custom_shortcodes_dir = trailingslashit( get_stylesheet_directory() ) 
 
 require_once $tttc_acf_custom_shortcodes_dir . '/gallery_shortcode/gallery_shortcode.php';
 require_once $tttc_acf_custom_shortcodes_dir . '/repeater_shortcode/repeater_shortcode.php';
+require_once $tttc_acf_custom_shortcodes_dir . '/tttc_acf_current_post_text_shortcode/tttc_acf_current_post_text_shortcode.php';
