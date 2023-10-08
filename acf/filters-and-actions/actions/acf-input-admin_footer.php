630087ae9c7f805129af9c1b6d4cd047e69c66a2
Alvin Muthui <alvinmuthui@gmail.com>
2023-10-08 21:34:53 +0300

    date picker minDate and maxDate

--- acf/filters-and-actions/actions/acf-input-admin_footer.php
+++ acf/filters-and-actions/actions/acf-input-admin_footer.php
@@ -16,5 +16,22 @@ function tttc_date_picker_input() {
 	acf.add_filter('date_picker_args', extend_date_range);//https://www.advancedcustomfields.com/resources/javascript-api/#filters-date_picker_args
 	acf.add_filter('date_time_picker_args', extend_date_range);//https://www.advancedcustomfields.com/resources/javascript-api/#filters-date_time_picker_args
 	</script>
+	<script type="text/javascript">
+	function tomorrow_min_date( args, field ){
+		// do something to args
+		args.minDate="+1d";// https://api.jqueryui.com/datepicker/#option-minDate
+		// return
+		return args;
+	}
+	acf.add_filter('date_picker_args', tomorrow_min_date);//https://www.advancedcustomfields.com/resources/javascript-api/#filters-date_picker_args
+	
+	function next_month_plus_one_day_max_date( args, field ){
+		// do something to args
+		args.maxDate="+1m +1d";// https://api.jqueryui.com/datepicker/#option-maxDate
+		// return
+		return args;
+	}
+	acf.add_filter('date_picker_args', next_month_plus_one_day_max_date);//https://www.advancedcustomfields.com/resources/javascript-api/#filters-date_picker_args
+	</script>
 	<?php
 }
