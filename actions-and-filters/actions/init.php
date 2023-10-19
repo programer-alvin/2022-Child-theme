399d03703e22df798729f61aacd8991feedd26c8
Alvin Muthui <alvinmuthui@gmail.com>
2023-10-19 19:29:30 +0300

    removed category from pages

--- actions-and-filters/actions/init.php
+++ actions-and-filters/actions/init.php
@@ -2,4 +2,4 @@
 function tttc_add_categories_to_pages() {
     register_taxonomy_for_object_type('category', 'page');
 }
-add_action('init', 'tttc_add_categories_to_pages');
+//add_action('init', 'tttc_add_categories_to_pages');
