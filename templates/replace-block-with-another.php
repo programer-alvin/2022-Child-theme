836761f9953a8675e51b93a254f8a18e049f1c66
Alvin Muthui <alvinmuthui@gmail.com>
2023-11-06 09:30:59 +0300

    new block on fly

--- templates/replace-block-with-another.php
+++ templates/replace-block-with-another.php
@@ -68,15 +68,19 @@
     
     $all_fields_block_acf_array_Data=convertJSONtoArray($all_fields_block_acf_json_Data);
     var_export($all_fields_block_acf_array_Data);//proccess this data if need*/
+    
+    //you can also create block on fly 
+    //$new_block='<!-- wp:acf/auto-block-gallery {"name":"acf/auto-block-gallery",'.$all_fields_block_acf_json_Data.' ,"mode":"preview","alignText":"left"} /-->';
 
     //Grid block
     $grid_block_content=retrieveAcfGridBlock($existing_content);
     $grid_block_content= replaceAcfData($grid_block_content, $all_fields_block_acfData);//replace data. It might need further processing before replacing if the fields are not the same.
 
 
-    $new_content        = $block_data . $grid_block_content;// append the block and delete the original block from backend if needed.
+    $new_content        = $existing_content . $grid_block_content;// append the block and delete the original block from backend if needed.
+    // $new_content        = $block_data . $new_block;
 
-    $post->post_content = $new_content;
+    $post->post_content = $existing_content;
     wp_update_post( $post );
 ?>
 <?php
