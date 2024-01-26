6f4dbd10f01918840291c83f1184402e94c08df4
Alvin Muthui <alvinmuthui@gmail.com>
2024-01-26 08:55:14 +0300

    added tttc_has_suffix function

--- extended-functions.php
+++ extended-functions.php
@@ -1,13 +1,21 @@
 <?php
 
 // Function to require all PHP files in a directory and its subdirectories
-function tttc_require_all_PHP_files_directory($directory) {
-    $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory));
-    $phpFiles = new RegexIterator($iterator, '/\.php$/');
+function tttc_require_all_PHP_files_directory( $directory ) {
+	$iterator = new RecursiveIteratorIterator( new RecursiveDirectoryIterator( $directory ) );
+	$phpFiles = new RegexIterator( $iterator, '/\.php$/' );
 
-    foreach ($phpFiles as $phpFile) {
-        require_once $phpFile->getPathname();
-    }
+	foreach ( $phpFiles as $phpFile ) {
+		require_once $phpFile->getPathname();
+	}
+}
+
+function tttc_has_suffix( $suffix, $content ) {
+	// Use the `substr` function to extract the substring with length equal to the length of the suffix
+	$contentSuffix = substr( $content, -strlen( $suffix ) );
+
+	// Compare the extracted suffix with the provided suffix
+	return $contentSuffix === $suffix;
 }
 
 function tttc_has_prefix( $prefix, $content ) {
@@ -21,11 +29,11 @@ function tttc_has_prefix( $prefix, $content ) {
 add_filter(
 	'sanitize_file_name',
 	function( string $filename, string $filename_raw ) {
-		//exclude ACF local files from the custom logic.
-		if ( tttc_has_prefix( 'group_', $filename ) || tttc_has_prefix( 'post_type_', $filename ) || tttc_has_prefix( 'taxonomy_', $filename ) || tttc_has_prefix( 'ui_options_page_', $filename ) ) {
+		// exclude ACF local files from the custom logic.
+		if ( tttc_has_suffix( '.json', $filename ) && ( tttc_has_prefix( 'group_', $filename ) || tttc_has_prefix( 'post_type_', $filename ) || tttc_has_prefix( 'taxonomy_', $filename ) || tttc_has_prefix( 'ui_options_page_', $filename ) ) ) {
 			return $filename;// the file is returned as it is
 		}
-		//Do something here.
+		// Do something here.
 		return $filename;
 	},
 	10,
